e45333dbcaaab2e2789ac07d40d1b4911c6b9fde
Dikry720p <89627365+Dikry720p@users.noreply.github.com>
2023-06-26 16:04:57 +0700

    Update read.php

--- api/employee/read.php
+++ api/employee/read.php
@@ -34,7 +34,7 @@ else {
 $items = new Employee($db);
 $stmt = $items->getEmployee();
 $itemCount = $stmt->rowCount();
-echo json_encode($itemCount);
+
 if($itemCount > 0){
 $employeeArr = array();
 $employeeArr["body"] = array();
